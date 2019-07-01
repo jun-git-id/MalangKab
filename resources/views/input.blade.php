5b008f60627ee4c143c278eb128984a097604099
Immanuela Christiantari <immanuelacip@gmail.com>
2019-07-01 16:32:45 +0700

    input tempat usaha

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="container w-50">
-        <form>
+        <form enctype="multipart/form-data">
             <div class="form-group">
                 <label for="formGroupExampleInput">Nama Toko</label>
                 <div class="input-group-prepend">
@@ -14,17 +14,19 @@
             </div>
             <div class="form-group">
                 <label for="formGroupExampleInput">Tambah Foto</label>
-                <div class="input-group-prepend">
-                    <span class="input-group-text no-border-right"><i class="fas fa-camera"></i></span>
-                    <input type="text" class="form-control no-border-left" id="formGroupExampleInput"
-                           placeholder="Foto">
+                <div class="form-group inputDnD">
+                    <label class="sr-only" for="inputFile">File Upload</label>
+                    <input type="file" class="form-control-file text-primary font-weight-bold py-5" id="inputFile"
+                           accept="image/*" onchange="readUrl(this)" data-title="Drag and drop a file">
                 </div>
             </div>
+
             <div class="form-group">
                 <label for="formGroupExampleInput">Alamat Toko</label>
                 <div class="input-group-prepend">
                     <span class="input-group-text no-border-right"><i class="fas fa-home"></i></span>
-                    <textarea placeholder="Alamat toko" class="form-control no-border-left" id="formGroupExampleInput"></textarea>
+                    <textarea placeholder="Alamat toko" class="form-control no-border-left"
+                              id="formGroupExampleInput"></textarea>
                 </div>
             </div>
             <div class="form-group">
@@ -47,7 +49,7 @@
                 <label for="formGroupExampleInput">Nomor Telepon</label>
                 <div class="input-group-prepend">
                     <span class="input-group-text no-border-right"><i class="fas fa-phone"></i></span>
-                    <input type="text" class="form-control no-border-left" id="formGroupExampleInput"
+                    <input type="number" class="form-control no-border-left" id="formGroupExampleInput"
                            placeholder="Nomor telepon">
                 </div>
             </div>
@@ -100,6 +102,53 @@
                     </select>
                 </div>
             </div>
+            <div class="form-group">
+                <label for="formGroupExampleInput">Nominal</label>
+                <div class="input-group-prepend">
+                    <span class="input-group-text no-border-right">Rp.</span>
+                    <input type="number" class="form-control no-border-left" id="formGroupExampleInput"
+                           placeholder="Nominal">
+                </div>
+            </div>
+            <h3 class="py-4">Detail Izin Usaha</h3>
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">Jenis Izin Usaha</label>
+                        <div class="input-group-prepend">
+                            <span class="input-group-text no-border-right"><i class="fas fa-clipboard-check"></i></span>
+                            <select class="custom-select" id="inputGroupSelect01">
+                                <option selected>Choose...</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">Nomor Izin Usaha</label>
+                        <div class="input-group-prepend">
+                            <span class="input-group-text no-border-right">No.</i></span>
+                            <input type="number" class="form-control no-border-left" id="formGroupExampleInput"
+                                   placeholder="nomor">
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label for="formGroupExampleInput">Tanggal Izin Berakhir</label>
+                        <div class="input-group-prepend">
+                            <span class="input-group-text no-border-right"><i class="fas fa-calendar-alt"></i></span>
+                            <input type="date" class="form-control no-border-left"
+                                   placeholder="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button type="button" class="btn btn-info">Tambah Izin Usaha</button>
         </form>
+        <button type="button" class="btn btn-info mt-4 w-100">Tambah Toko</button>
     </div>
 @endsection
