b424969ed0ad21843fb86444f36d8bbdde6b7aba
Immanuela Christiantari <immanuelacip@gmail.com>
2019-07-25 12:25:01 +0700

    error pull

--- resources/views/detailproduk.blade.php
+++ resources/views/detailproduk.blade.php
@@ -11,40 +11,40 @@
 <body>
 
 <div class="container">
-  <h2>Carousel Example</h2>  
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
+    <h2>Carousel Example</h2>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-      <div class="item active">
-        <img src="img/kebun.jpg" alt="Los Angeles" style="width:30%;">
-      </div>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="img/kebun.jpg" alt="Los Angeles" style="width:30%;">
+            </div>
 
-      <div class="item">
-        <img src="img/kebun.jpg" alt="Chicago" style="width:30%;">
-      </div>
-    
-      <div class="item">
-        <img src="img/kebun.jpg" alt="New york" style="width:30%;">
-      </div>
+            <div class="item">
+                <img src="img/kebun.jpg" alt="Chicago" style="width:30%;">
+            </div>
+
+            <div class="item">
+                <img src="img/kebun.jpg" alt="New york" style="width:30%;">
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
 </div>
 
 </body>
